6fda53f3fa0c245678528d1e0bbfe93a791a35f5
John nadar <john.nadar@lntinfotech.com>
2022-11-14 15:18:45 +0000

    Merging initial project structure into dev.

---
+++ db-service/src/test/java/in/gov/abdm/nmr/DbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.gov.abdm.nmr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
