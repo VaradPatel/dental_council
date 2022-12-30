2eb926b1b00f4b8be81193b6315e771d6fbadd41
saurabh.ghanwat@lntinfotech.com <saurabh.ghanwat@lntinfotech.com>
2022-12-30 18:27:55 +0530

    Implemented reset password API for forgot password implementation

---
+++ src/main/java/in/gov/abdm/nmr/controller/ResetPasswordController.java
@@ -0,0 +1,33 @@
+package in.gov.abdm.nmr.controller;
+
+import in.gov.abdm.nmr.dto.ResetPasswordRequestTo;
+import in.gov.abdm.nmr.dto.ResetPasswordResponseTo;
+import in.gov.abdm.nmr.service.IResetPasswordService;
+import in.gov.abdm.nmr.util.NMRConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+/**
+ * Controller for resetting user password
+ */
+@RestController
+public class ResetPasswordController {
+
+    @Autowired
+	IResetPasswordService resetPasswordService;
+
+	/**
+	 * API Endpoint to reset password
+	 * @param resetPasswordRequestTo coming from user
+	 * @return ResetPasswordResponseTo object
+	 */
+	@PostMapping(path = NMRConstants.RESET_PASSWORD, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResetPasswordResponseTo resetPassword(@Valid @RequestBody ResetPasswordRequestTo resetPasswordRequestTo) {
+		return resetPasswordService.resetPassword(resetPasswordRequestTo);
+	}
+}
