073e149c7a63418b1f20619ab243f2f76b8e956b
saurabh.ghanwat@lntinfotech.com <saurabh.ghanwat@lntinfotech.com>
2023-01-25 11:44:21 +0530

    Doctor login fixed and compilation issues resolved

--- src/main/java/in/gov/abdm/nmr/service/impl/ActionServiceImpl.java
+++ src/main/java/in/gov/abdm/nmr/service/impl/ActionServiceImpl.java
@@ -31,7 +31,7 @@ public class ActionServiceImpl implements IActionService {
     @Autowired
     IQualificationDetailRepository iQualificationDetailRepository;
     @Autowired
-    ICustomQualificationDetailRepository iCustomQualificationDetailRepository;
+    IForeignQualificationDetailRepository iCustomQualificationDetailRepository;
     @Autowired
     SuperSpecialityRepository superSpecialityRepository;
     @Autowired
@@ -112,10 +112,10 @@ public class ActionServiceImpl implements IActionService {
         }
         iQualificationDetailRepository.saveAll(qualificationDetails);
 
-        List<CustomQualificationDetails> customQualificationDetailsList = new ArrayList<>();
-        List<CustomQualificationDetails> customQualificationDetails = iCustomQualificationDetailRepository.getQualificationDetailsByHpProfileId(existingHpProfile.getId());
-        for (CustomQualificationDetails customQualificationDetail : customQualificationDetails) {
-            CustomQualificationDetails newCustomQualificationDetails = new CustomQualificationDetails();
+        List<ForeignQualificationDetails> customQualificationDetailsList = new ArrayList<>();
+        List<ForeignQualificationDetails> customQualificationDetails = iCustomQualificationDetailRepository.getQualificationDetailsByHpProfileId(existingHpProfile.getId());
+        for (ForeignQualificationDetails customQualificationDetail : customQualificationDetails) {
+            ForeignQualificationDetails newCustomQualificationDetails = new ForeignQualificationDetails();
             org.springframework.beans.BeanUtils.copyProperties(customQualificationDetail, newCustomQualificationDetails);
             newCustomQualificationDetails.setId(null);
             newCustomQualificationDetails.setHpProfile(targetedHpProfile);
