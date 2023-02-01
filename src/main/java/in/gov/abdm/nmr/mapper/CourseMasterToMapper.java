73ca475e59f1dea762f96c76b7b560722a4e8b08
saurabh.ghanwat@lntinfotech.com <saurabh.ghanwat@lntinfotech.com>
2023-02-01 14:48:18 +0530

    courses issue resolved

---
+++ src/main/java/in/gov/abdm/nmr/mapper/CourseMasterToMapper.java
@@ -0,0 +1,29 @@
+package in.gov.abdm.nmr.mapper;
+
+import in.gov.abdm.nmr.dto.*;
+import in.gov.abdm.nmr.dto.masterdata.MasterDataTO;
+import lombok.experimental.UtilityClass;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@UtilityClass
+public final class CourseMasterToMapper {
+
+    public static List<MasterDataTO> courseToListToCourseMasterToList(List<CourseTO> course) {
+        List<MasterDataTO> list = new ArrayList<>();
+        course.forEach(c -> {
+            list.add(courseToMaster(c));
+        });
+        return list;
+    }
+
+    MasterDataTO courseToMaster(CourseTO courseTO) {
+        MasterDataTO masterDataTO = new MasterDataTO();
+        masterDataTO.setId(courseTO.getId().longValue());
+        masterDataTO.setName(courseTO.getCourseName());
+        System.out.println(masterDataTO);
+        return masterDataTO;
+    }
+
+}
