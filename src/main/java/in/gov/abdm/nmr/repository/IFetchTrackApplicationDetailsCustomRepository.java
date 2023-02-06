90034bef01a0b1d1e3d3a8f3c292e5704d3505ca
john.nadar@lntinfotech.com <john.nadar@lntinfotech.com>
2023-02-06 15:53:17 +0530

    Fixed captcha algorithm.

--- src/main/java/in/gov/abdm/nmr/repository/IFetchTrackApplicationDetailsCustomRepository.java
+++ src/main/java/in/gov/abdm/nmr/repository/IFetchTrackApplicationDetailsCustomRepository.java
@@ -1,6 +1,6 @@
 package in.gov.abdm.nmr.repository;
 
-import in.gov.abdm.nmr.dto.HealthProfessionalApplicationRequestParamsTo;;
+import in.gov.abdm.nmr.dto.HealthProfessionalApplicationRequestParamsTo;
 import in.gov.abdm.nmr.dto.HealthProfessionalApplicationResponseTo;
 import org.springframework.data.domain.Pageable;
 
