971ebce977d58351311dec293613e6c67c7ab33b
john.nadar@lntinfotech.com <john.nadar@lntinfotech.com>
2023-02-17 18:06:02 +0530

    Changed the request body to request params.

--- src/main/java/in/gov/abdm/nmr/service/ISearchService.java
+++ src/main/java/in/gov/abdm/nmr/service/ISearchService.java
@@ -2,6 +2,8 @@ package in.gov.abdm.nmr.service;
 
 import java.math.BigInteger;
 
+import org.springframework.data.domain.Pageable;
+
 import in.gov.abdm.nmr.dto.HpSearchProfileTO;
 import in.gov.abdm.nmr.dto.HpSearchRequestTO;
 import in.gov.abdm.nmr.dto.HpSearchResponseTO;
@@ -9,7 +11,7 @@ import in.gov.abdm.nmr.exception.NmrException;
 
 public interface ISearchService {
 
-    HpSearchResponseTO searchHP(HpSearchRequestTO hpSearchRequestTO) throws NmrException;
+    HpSearchResponseTO searchHP(HpSearchRequestTO hpSearchRequestTO, Pageable pageable) throws NmrException;
 
     HpSearchProfileTO getHpSearchProfileById(BigInteger profileId) throws NmrException;
 }
