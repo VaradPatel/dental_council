55bbc7f10fb1c45b571d98fe780f673ea569e9f1
10717747 <govind.kedia@lntinfotech.com>
2023-03-19 23:04:09 +0530

    Added null check

--- src/test/java/in/gov/abdm/nmr/service/impl/HpRegistrationServiceImplTest.java
+++ src/test/java/in/gov/abdm/nmr/service/impl/HpRegistrationServiceImplTest.java
@@ -7,6 +7,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+
 @ExtendWith(MockitoExtension.class)
 public class HpRegistrationServiceImplTest {
 
@@ -14,11 +20,17 @@ public class HpRegistrationServiceImplTest {
     HpRegistrationServiceImpl hpRegistrationService;
 
     @Test
-    public void testFuzzyScore(){
+    public void testFuzzyScore() throws IOException {
 
         double fuzzyScore = hpRegistrationService.getFuzzyScore("Satish Chonde", "Satish Udhavrao chonde");
 //        System.out.println(fuzzyScore);
-        Assertions.assertTrue(fuzzyScore> 80);
+//        Assertions.assertTrue(fuzzyScore> 80);
+
+        File file = new File("C:/Users/10717747/Downloads/e-sign_NMR.pdf");
+        byte[] input_file = Files.readAllBytes(Paths.get("C:/Users/10717747/Downloads/e-sign_NMR.pdf"));
+//        Base64.getEncoder().encodeToString(input_file);
+        String encodeToString = Base64.getEncoder().encodeToString(input_file);
+        Base64.getDecoder().decode(encodeToString)
     }
 
 }
