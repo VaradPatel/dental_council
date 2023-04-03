4d618154b84e5d0d3b543257f33a2eaaa1da2436
10696279 <pravin.salunke@lntinfotech.com>
2023-04-03 12:36:27 +0530

    kafka implementation correction and Added Gateway URL in config

--- src/main/java/in/gov/abdm/nmr/client/GatewayFClient.java
+++ src/main/java/in/gov/abdm/nmr/client/GatewayFClient.java
@@ -2,16 +2,23 @@ package in.gov.abdm.nmr.client;
 
 import in.gov.abdm.nmr.dto.SessionRequestTo;
 import in.gov.abdm.nmr.dto.SessionResponseTo;
+import in.gov.abdm.nmr.util.NMRConstants;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 /**
- * Client to call Aadhaar Service APIS
+ * Client to call Session Service APIs through the Gateway service.
  */
-@FeignClient(name = "gateway", url = "https://preprod.abdm.gov.in")
+@FeignClient(name = NMRConstants.GATEWAY_SERVICE, url = NMRConstants.GATEWAY_SERVICE_ENDPOINT)
 public interface GatewayFClient {
 
-    @PostMapping(value = "/gateway/v0.5/sessions")
+    /**
+     * Calls the /session endpoint of the Gateway service to create a new session.
+     *
+     * @param sessionRequestTo the request body containing session details.
+     * @return a {@link SessionResponseTo} object containing the response details
+     */
+    @PostMapping(value = NMRConstants.SESSION_URL)
     SessionResponseTo sessions(@RequestBody SessionRequestTo sessionRequestTo);
 }
