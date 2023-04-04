2dd72f7d4a6a220c33e633c49462c7623f61f6f8
SaiKrishna Kande <saikrishna.kande@lntinfotech.com>
2023-04-04 20:08:50 +0530

    fetch doctor details from mongodb changes

--- src/main/java/in/gov/abdm/nmr/service/IWorkflowPostProcessorService.java
+++ src/main/java/in/gov/abdm/nmr/service/IWorkflowPostProcessorService.java
@@ -1,9 +1,9 @@
 package in.gov.abdm.nmr.service;
 
 import in.gov.abdm.nmr.dto.WorkFlowRequestTO;
-import in.gov.abdm.nmr.entity.HpProfile;
-import in.gov.abdm.nmr.entity.HpProfileMaster;
-import in.gov.abdm.nmr.entity.WorkFlow;
+import in.gov.abdm.nmr.jpa.entity.HpProfile;
+import in.gov.abdm.nmr.jpa.entity.HpProfileMaster;
+import in.gov.abdm.nmr.jpa.entity.WorkFlow;
 import in.gov.abdm.nmr.exception.WorkFlowException;
 import in.gov.abdm.nmr.mapper.INextGroup;
 
