297d659e6347256f1a5c7b2a9a3fca0f862d944e
10696279 <pravin.salunke@lntinfotech.com>
2023-04-19 17:04:54 +0530

    CS-1651 Issue Fixes (District ,State ,subdistrict APIs response in Alphabetical order)

--- src/main/java/in/gov/abdm/nmr/repository/DistrictRepository.java
+++ src/main/java/in/gov/abdm/nmr/repository/DistrictRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface DistrictRepository extends JpaRepository<District, BigInteger> {
 
-    @Query(value = "SELECT id, name, iso_code, state_id, created_at, updated_at FROM district where state_id=:state", nativeQuery = true)
+    @Query(value = "SELECT id, name, iso_code, state_id, created_at, updated_at FROM district where state_id=:state order by name asc", nativeQuery = true)
     List<District> getDistrict(BigInteger state);
 
     District findByName(String name);
