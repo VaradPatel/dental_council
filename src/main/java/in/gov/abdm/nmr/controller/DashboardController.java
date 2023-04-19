8f376575ebb40d200e60cefa35269245e2439ae6
10696279 <pravin.salunke@lntinfotech.com>
2023-04-19 15:25:52 +0530

    removed unwanted code.

--- src/main/java/in/gov/abdm/nmr/controller/DashboardController.java
+++ src/main/java/in/gov/abdm/nmr/controller/DashboardController.java
@@ -1,6 +1,5 @@
 package in.gov.abdm.nmr.controller;
 
-import in.gov.abdm.nmr.dto.DashboardRequestTO;
 import in.gov.abdm.nmr.dto.DashboardResponseTO;
 import in.gov.abdm.nmr.dto.FetchCountOnCardResponseTO;
 import in.gov.abdm.nmr.exception.InvalidRequestException;
@@ -9,7 +8,10 @@ import in.gov.abdm.nmr.service.IFetchCountOnCardService;
 import in.gov.abdm.nmr.service.IFetchSpecificDetailsService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.nio.file.AccessDeniedException;
 
@@ -46,23 +48,6 @@ public class DashboardController {
     @SecurityRequirement(name = "bearerAuth")
     public FetchCountOnCardResponseTO fetchCountOnCard() throws AccessDeniedException {
         return iFetchCountOnCardService.fetchCountOnCard();
-    }
-
-    /**
-     * This endpoint can be accessed to retrieve specific details based on the Registration Number
-     * @return List<FetchSpecificDetailsResponseTO>
-     */
-
-    /**
-     * This API endpoint is used to retrieve Dashboard specific request details.
-     * @param requestTO - The request object containing the necessary parameters to fetch the dashboard details.
-     * @return The response object with request specific details related to the dashboard.
-     * @throws InvalidRequestException when the request is invalid.
-     */
-    @Deprecated
-    @PostMapping(PATH_DASHBOARD_FETCH_DETAILS)
-    public DashboardResponseTO fetchDashboardData(@RequestBody DashboardRequestTO requestTO) throws InvalidRequestException {
-        return iFetchSpecificDetailsService.fetchDashboardData(requestTO);
     }
 
     /**
