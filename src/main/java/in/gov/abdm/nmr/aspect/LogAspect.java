60ebdb6b6e99d37deef1ad3d2eefa63cffe849c5
10717747 <govind.kedia@lntinfotech.com>
2023-05-25 23:49:33 +0530

    Removed Mono and Flux implementation from log aspect, as project is non-reactive.

--- src/main/java/in/gov/abdm/nmr/aspect/LogAspect.java
+++ src/main/java/in/gov/abdm/nmr/aspect/LogAspect.java
@@ -6,11 +6,8 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
-import java.util.Objects;
 
 @Aspect
 @Component
