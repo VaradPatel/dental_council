953e744b71b9d10cd6f1ab6504e788604f5e2b75
john.nadar@lntinfotech.com <john.nadar@lntinfotech.com>
2023-07-06 20:57:41 +0530

    1. Changed precedence of filter to highest to log request length. 2. Overrode log level from code.

--- src/main/java/in/gov/abdm/nmr/config/TraceFilter.java
+++ src/main/java/in/gov/abdm/nmr/config/TraceFilter.java
@@ -4,6 +4,8 @@ import brave.Tracer;
 import in.gov.abdm.nmr.common.CustomHeaders;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @Component
 public class TraceFilter extends OncePerRequestFilter {
 
@@ -27,6 +30,7 @@ public class TraceFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         LOGGER.info("Adding trace-id to response");
+        LOGGER.info("Request size {}", request.getContentLength());
         response.addHeader(CustomHeaders.CORRELATION_ID, tracer.currentSpan().context().traceIdString());
         super.doFilter(request, response, filterChain);
     }
