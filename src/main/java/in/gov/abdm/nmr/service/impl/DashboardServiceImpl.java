64818ac69c07a1a9d23be8fa9853cf77f795fa2d
10717747 <govind.kedia@lntinfotech.com>
2023-07-08 15:55:19 +0530

    Fixed ordering on card details. Records will be fetched in descending order of creation meaning,latest application will come first.

--- src/main/java/in/gov/abdm/nmr/service/impl/DashboardServiceImpl.java
+++ src/main/java/in/gov/abdm/nmr/service/impl/DashboardServiceImpl.java
@@ -280,11 +280,11 @@ public class DashboardServiceImpl implements IDashboardService {
         columnToSortMap.put("hpProfileId", " calculate.hp_profile_id");
         columnToSortMap.put("requestId", " calculate.request_id");
         columnToSortMap.put("registrationNo", " rd.registration_no");
-        columnToSortMap.put("createdAt", " rd.created_at");
+        columnToSortMap.put("createdAt", " d.created_at");
         columnToSortMap.put("councilName", " stmc.name");
         columnToSortMap.put("applicantFullName", " hp.full_name");
         columnToSortMap.put("pendency", " pendency");
-        return columnToSortMap.getOrDefault(columnToSort, " pendency ");
+        return columnToSortMap.getOrDefault(columnToSort, " d.created_at ");
     }
 
     private void setApplicationTypeIdAndUserGroupStatus(String applicationTypeId, String userGroupStatus,
