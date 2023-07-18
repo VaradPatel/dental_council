7d74aa771837da1f3fa525b4f5d6f68be47b5520
saurabh.ghanwat@lntinfotech.com <saurabh.ghanwat@lntinfotech.com>
2023-07-18 11:39:30 +0530

    Remark message updated to "Rejected by system due to profile suspension"

--- src/main/java/in/gov/abdm/nmr/util/NMRConstants.java
+++ src/main/java/in/gov/abdm/nmr/util/NMRConstants.java
@@ -351,7 +351,7 @@ public class NMRConstants {
     public static final String COLLEGE_NBE_VERIFIED = "College/NBE Verified";
     public static final String SUBMITTED = "Submitted";
     public static final String VERIFIED =  "Verified";
-    public static final String SYSTEM_REJECTION_REMARK =  "System rejected because of profile suspension";
+    public static final String SYSTEM_REJECTION_REMARK =  "Rejected by system due to profile suspension";
 
 
     public static final String APPLICATION_REQUEST_DETAILS = """
