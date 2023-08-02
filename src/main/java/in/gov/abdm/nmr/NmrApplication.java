1696f84bc622d86e7346893aa194aeed71dd96f1
saurabh.ghanwat@lntinfotech.com <saurabh.ghanwat@lntinfotech.com>
2023-08-02 16:46:06 +0530

    caching mechanism added for notification-db template fetch call

--- src/main/java/in/gov/abdm/nmr/NmrApplication.java
+++ src/main/java/in/gov/abdm/nmr/NmrApplication.java
@@ -2,6 +2,7 @@ package in.gov.abdm.nmr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -9,6 +10,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableCaching
 @EnableAsync
 @SpringBootApplication
 @EnableJpaAuditing
