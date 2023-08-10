0bf216a3889a1e739822cc52afe918c04332e529
10696279 <pravin.salunke@lntinfotech.com>
2023-08-10 10:59:25 +0530

    changes for master data for integration with hpr.

--- src/main/java/in/gov/abdm/nmr/repository/CourseRepository.java
+++ src/main/java/in/gov/abdm/nmr/repository/CourseRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface CourseRepository extends JpaRepository<Course, BigInteger> {
 
-    @Query(value = "SELECT course_name, id, hpr_course_id FROM course", nativeQuery = true)
+    @Query(value = "SELECT course_name, id FROM course", nativeQuery = true)
     List<Course> getCourse();
 
     Course getByCourseName(String courseName);
