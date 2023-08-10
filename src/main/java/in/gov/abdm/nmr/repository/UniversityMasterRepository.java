a2c5b3b835ac83267d3e02320d08b1109d63ff2f
10717747 <govind.kedia@lntinfotech.com>
2023-08-10 19:11:25 +0530

    Added filter condition for universities.

--- src/main/java/in/gov/abdm/nmr/repository/UniversityMasterRepository.java
+++ src/main/java/in/gov/abdm/nmr/repository/UniversityMasterRepository.java
@@ -8,16 +8,16 @@ import java.util.List;
 
 @Repository
 public interface UniversityMasterRepository extends JpaRepository<UniversityMaster, BigInteger> {
-    @Query(value = "SELECT distinct on (name) name,id,status ,visible_status ,college_id, created_at, updated_at FROM university_master WHERE  college_id=:collegeId", nativeQuery = true)
+    @Query(value = "SELECT distinct on (name) name,id,status ,visible_status ,college_id, created_at, updated_at FROM university_master WHERE  college_id=:collegeId and visible_status = 1", nativeQuery = true)
     List<UniversityMaster> getUniversitiesByCollegeId(BigInteger collegeId);
 
     @Query(value = "SELECT * FROM university_master WHERE id=:id", nativeQuery = true)
     UniversityMaster findUniversityMasterById(BigInteger id);
 
-    @Query(value = "SELECT distinct on (name) name ,id,status ,visible_status ,college_id ,created_at ,updated_at FROM university_master", nativeQuery = true)
+    @Query(value = "SELECT distinct on (name) name ,id,status ,visible_status ,college_id ,created_at ,updated_at FROM university_master where visible_status = 1", nativeQuery = true)
     List<UniversityMaster> getUniversities();
 
-    @Query(value = "SELECT id, name, status, visible_status, college_id, created_at, updated_at FROM university_master WHERE name ilike :universityName and college_id=:collegeId LIMIT 1",
+    @Query(value = "SELECT id, name, status, visible_status, college_id, created_at, updated_at FROM university_master WHERE name ilike :universityName and college_id=:collegeId and visible_status = 1 LIMIT 1",
             nativeQuery = true)
     UniversityMaster findUniversityByName(String universityName, BigInteger collegeId);
 }
