06ed764d3ec94e20237c4e85cb0144b1f5312d59
10717747 <govind.kedia@lntinfotech.com>
2023-08-19 23:55:00 +0530

    Added ContentCachingFilter to read http request multiple times. it has been observed with slow network occasionallhy request are failing and on analysis, we could include its due to empty request body. The request body can be empty only when a input stream was already read by some filter, to prevent this to happen, have added ContentCachingFilter with highest precedence and have cached the content .

---
+++ src/main/java/in/gov/abdm/nmr/config/ContentCachingFilter.java
@@ -0,0 +1,29 @@
+package in.gov.abdm.nmr.config;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Order(value = Ordered.HIGHEST_PRECEDENCE)
+@Component
+@Slf4j
+@WebFilter(filterName = "ContentCachingFilter", urlPatterns = "/user/login")
+public class ContentCachingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        log.info("in content caching filter");
+        CachedBodyHttpServletRequest cachedBodyHttpServletRequest = new CachedBodyHttpServletRequest(httpServletRequest);
+        filterChain.doFilter(cachedBodyHttpServletRequest, httpServletResponse);
+    }
+}
